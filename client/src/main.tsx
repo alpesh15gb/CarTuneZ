47dd26fc6185d34c41a0d0f415347c1fd52e4c86
alpesh15gb <alpesh15gb@gmail.com>
2025-08-28 23:01:51 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,1 +1,6 @@
 import './three/setup'
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(<App />);
