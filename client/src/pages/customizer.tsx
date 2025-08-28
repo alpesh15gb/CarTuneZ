8cadada38d4302cc56e25fdf355b00a8d9b6e885
alpesh15gb <alpesh15gb@gmail.com>
2025-08-28 23:07:12 +0530

    Create customizer.tsx

---
+++ client/src/pages/customizer.tsx
@@ -0,0 +1,5 @@
+// client/src/pages/Customizer.tsx (example)
+import CarCanvas from '@/components/3d/CarCanvas'
+export default function CustomizerPage() {
+  return <div className="h-[calc(100vh-64px)]"><CarCanvas /></div>
+}
